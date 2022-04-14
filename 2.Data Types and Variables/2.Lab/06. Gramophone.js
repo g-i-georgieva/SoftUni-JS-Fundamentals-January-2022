fa87b842b07dbb3f892f1b781b922230cc016158
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-04-14 09:16:50 +0300

    Delete 06. Gramophone.js

--- 2.Data Types and Variables/2.Lab/06. Gramophone.js
+++
@@ -1,7 +0,0 @@
-function rotate(brand, album, song) {
-    let time = (album.length * brand.length) * song.length / 2
-    let rotations = Math.ceil(time / 2.5);
-
-    console.log(`The plate was rotated ${rotations} times.`)
-}
-rotate('Rammstein', 'Sehnsucht', 'Engel')
