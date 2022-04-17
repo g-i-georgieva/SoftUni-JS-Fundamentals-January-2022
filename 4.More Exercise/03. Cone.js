0bcf7ca672de214f53a7b9365107c63e45db1150
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-04-17 21:23:24 +0300

    Delete 4.More Exercise directory

--- 4.More Exercise/03. Cone.js
+++
@@ -1,5 +0,0 @@
-function cone(radius, high) {
-    console.log(`volume = ${((1 / 3) * Math.PI * (radius * radius) * high).toFixed(4)}`)
-    console.log(`area = ${(Math.PI * radius * (radius + Math.sqrt(radius * radius + high * high))).toFixed(4)}`)
-}
-cone(3, 5)
--- 4.More Exercise/05. Binary to Decimal.js
+++
@@ -1,6 +0,0 @@
-function binaryToDecimal(num) {
-    let bin = parseInt(num, 2)
-
-    console.log(bin)
-}
-binaryToDecimal(00001001)
