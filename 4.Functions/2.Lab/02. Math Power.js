a9890535e33f78a4528813c78fb841006c6b9c2a
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-04-20 09:47:06 +0300

    Add files via upload

---
+++ 4.Functions/2.Lab/02. Math Power.js
@@ -0,0 +1,9 @@
+function mathPower(num, x) {
+    // let result = 1
+    // for (let i = 1; i <= x; i++) {
+    //     result *=num
+    // }
+    let result = Math.pow(num, x)
+    console.log(result)
+}
+mathPower(2, 8)
