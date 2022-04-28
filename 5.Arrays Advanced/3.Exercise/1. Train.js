7b3dbd7400e60f60283a8bd2de75a268bcc98474
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-04-28 10:18:07 +0300

    Add files via upload

---
+++ 5.Arrays Advanced/3.Exercise/1. Train.js
@@ -0,0 +1,24 @@
+function train(input) {
+  let train = input.shift().split(" ").map(Number);
+  let maxCapacity = Number(input.shift());
+
+  for (let command of input) {
+    let tokens = command.split(" ");
+
+    if (tokens[0] === "Add") {
+      let passengers = Number(tokens[1]);
+      train.push(passengers);
+    } else {
+      let passengers = Number(tokens[0]);
+      for (let i = 0; i < train.length; i++) {
+        if (train[i] + passengers <= maxCapacity) {
+          train[i] += passengers;
+          break;
+        }
+      }
+    }
+  }
+
+  console.log(train.join(" "));
+}
+train(["32 54 21 12 4 0 23", "75", "Add 10", "Add 0", "30", "10", "75"]);
