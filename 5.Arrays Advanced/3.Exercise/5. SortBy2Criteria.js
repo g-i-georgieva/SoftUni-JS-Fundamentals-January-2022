0d5e452903246f8f2f361d75a57258a7fedd3243
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-05-01 20:37:02 +0300

    Add files via upload

---
+++ 5.Arrays Advanced/3.Exercise/5. SortBy2Criteria.js
@@ -0,0 +1,8 @@
+function solve(arr) {
+  
+    arr.sort((a, b) => a.length - b.length || a.localeCompare(b))
+
+    console.log(arr.join("\n"))
+}
+solve(["alpha", "beta", "gamma"]);
+solve(["Isacc", "Theodor", "Jack", "Harrison", "George"]);
