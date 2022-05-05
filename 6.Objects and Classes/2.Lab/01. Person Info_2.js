39a3db77f2c0d75b0ce9b02e56c624e7dcc15c34
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-05-05 12:46:27 +0300

    Add files via upload

---
+++ 6.Objects and Classes/2.Lab/01. Person Info_2.js
@@ -0,0 +1,9 @@
+function printPersonInfo(firstName, lastName, age) {
+  let personInfo = {
+    firstName,
+    lastName,
+    age,
+  };
+  return personInfo;
+}
+console.log(printPersonInfo("Peter", "Pan", "20"));
