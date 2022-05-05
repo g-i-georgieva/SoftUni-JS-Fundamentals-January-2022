7bc4b385baa195ca2eef775f829f5204109ad02b
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-05-05 12:45:53 +0300

    Delete 6.Objects and Classes directory

--- 6.Objects and Classes/1.Conditions/2.Lab/01. Person Info_2.js
+++
@@ -1,9 +0,0 @@
-function printPersonInfo(firstName, lastName, age) {
-  let personInfo = {
-    firstName,
-    lastName,
-    age,
-  };
-  return personInfo;
-}
-console.log(printPersonInfo("Peter", "Pan", "20"));
