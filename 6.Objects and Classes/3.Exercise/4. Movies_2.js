959f239b3c1e544c798aa1f2a12a401e584c3329
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-05-06 20:37:50 +0300

    Add files via upload

---
+++ 6.Objects and Classes/3.Exercise/4. Movies_2.js
@@ -0,0 +1,40 @@
+function solve(input) {
+  let listOfMoivies = [];
+  for (let lines of input) {
+    if (lines.includes("addMovie")) {
+      let nameOfMovie = lines.split("addMovie ")[1];
+      listOfMoivies.push({ name: nameOfMovie });
+    } else if (lines.includes("directedBy")) {
+      let info = lines.split(" directedBy ");
+      let name = info[0];
+      let director = info[1];
+      let movie = listOfMoivies.find((movieObj) => movieObj.name === name);
+      if (movie) {
+        movie.director === director;
+      }
+    } else if (lines.includes("onDate")) {
+      let info = lines.split(" onDate ");
+      let name = info[0];
+      let date = info[1];
+      let movie = listOfMoivies.find((movieObj) => movieObj.name === name);
+      if (movie) {
+        movie.date = date;
+      }
+    }
+  }
+  for (let movie of listOfMoivies) {
+      if (movie.name && movie.director && movie.date){
+        console.log(JSON.stringify(movie))
+      }
+  }
+}
+solve([
+  "addMovie Fast and Furious",
+  "addMovie Godfather",
+  "Inception directedBy Christopher Nolan",
+  "Godfather directedBy Francis Ford Coppola",
+  "Godfather onDate 29.07.2018",
+  "Fast and Furious onDate 30.07.2018",
+  "Batman onDate 01.08.2018",
+  "Fast and Furious directedBy Rob Cohen",
+]);
