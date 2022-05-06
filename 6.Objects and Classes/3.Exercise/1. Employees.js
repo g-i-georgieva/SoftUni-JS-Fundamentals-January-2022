1cecb3afea900fcfe21e8583e6649a8cd6c1b7ec
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-05-06 20:35:33 +0300

    Add files via upload

---
+++ 6.Objects and Classes/3.Exercise/1. Employees.js
@@ -0,0 +1,30 @@
+function employees(listOfEmployees) {
+  //pyrvo
+  //   for (let employee of listOfEmployees) {
+  //     let employeeData = {
+  //       name: employee,
+  //       personalNumber: employee.length,
+  //     };
+  //   console.log(`Name: ${name} -- Personal Number: ${name.length}`);
+  
+  //vtoro
+  // let employeeData = Object.entries(listOfEmployees);
+  // for (let [nameId, name] of employeeData) {
+  //   console.log(`Name: ${name} -- Personal Number: ${name.length}`);
+  // }
+
+  //treto
+  // let list = {}
+  // for (let employee of listOfEmployees) {
+  //   list.name = employee;
+  //   list.personalNumber = list.name.length //list.personalNumber = employee.length
+  //   console.log(list.personalNumber)
+  // }
+}
+
+employees([
+  "Silas Butler",
+  "Adnaan Buckley",
+  "Juan Peterson",
+  "Brendan Villarreal",
+]);
