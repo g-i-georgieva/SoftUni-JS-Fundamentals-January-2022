5c60ff8e805354856f3855f34a8c5d719ad48365
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-05-07 23:27:23 +0300

    Add files via upload

---
+++ 6.Objects and Classes/4.More Exercise/05. Sequences.js
@@ -0,0 +1,10 @@
+function sequences(input) {
+    input = input
+        .map(JSON.parse)
+        .map(el => el.sort((a, b) => b - a))
+        .map(JSON.stringify);
+    [...new Set(input)]
+        .map(JSON.parse)
+        .sort((a, b) => a.length - b.length)
+        .forEach(el => console.log(`[${el.join(', ')}]`));
+}
