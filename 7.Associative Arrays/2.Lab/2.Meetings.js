8261ecac5ea7dfecbdec4736c30d328a3e213f06
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-05-08 22:33:55 +0300

    Add files via upload

---
+++ 7.Associative Arrays/2.Lab/2.Meetings.js
@@ -0,0 +1,18 @@
+function meetings(input) {
+  let result = {};
+
+  for (let line of input) {
+    let [day, name] = line.split(" ");
+    if (result.hasOwnProperty(day)) {
+      console.log(`Conflict on ${day}!`);
+    } else {
+      result[day] = name;
+      console.log(`Scheduled for ${day}`);
+    }
+  }
+
+  for (let day in result) {
+    console.log(day, "->", result[day]);
+  }
+}
+meetings(["Monday Peter", "Wednesday Bill", "Monday Tim", "Friday Tim"]);
