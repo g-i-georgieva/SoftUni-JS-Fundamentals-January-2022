bf757a4d8b51bca1254efae826bd79536356475d
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-05-09 09:37:57 +0300

    Delete demo.js

--- 7.Associative Arrays/2.Lab/demo.js
+++
@@ -1,9 +0,0 @@
-let arr = {};
-arr["one"] = 1;
-arr["two"] = 2;
-arr["three"] = 3;
-
-// for(let key in assocArr) {
-//    console.log(key + " = " + assocArr[key]);
-// }
-Object.keys(arr).forEach((k) => console.log(`${k} -> ${arr[k]}`));
