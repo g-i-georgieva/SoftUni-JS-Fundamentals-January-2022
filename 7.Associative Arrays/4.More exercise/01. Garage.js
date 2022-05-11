12364b52316b794a019cbd386af3c4957403ec1a
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-05-11 10:27:06 +0300

    Add files via upload

---
+++ 7.Associative Arrays/4.More exercise/01. Garage.js
@@ -0,0 +1,24 @@
+function solveGarageArray(input) {
+    let garages = [];
+    for (const line of input) {
+        let [garageNumber, carInfo] = line.split(' - ');
+        let found = garages.find(g => g.garageNumber === garageNumber);
+        if (!found) {
+            garages.push({
+                garageNumber: garageNumber,
+                carInfo: []
+            });
+            found = garages.find(g => g.garageNumber === garageNumber);
+        }
+        found.carInfo.push(carInfo);
+    }
+    let output = '';
+    garages.forEach(garage => {
+        output += `Garage № ${garage.garageNumber}\n`;
+        for (let currCar of garage.carInfo) {
+            currCar = currCar.replace(/: /g, ' - ');
+            output += `--- ${currCar}\n`;
+        }
+    });
+    console.log(output);
+}
