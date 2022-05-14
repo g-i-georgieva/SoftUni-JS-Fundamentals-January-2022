656a966f0ac2257f77bc3e53fd812ffb6fc94cd5
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-05-14 22:03:52 +0300

    Add files via upload

---
+++ 8.Text processing/3.Exercise/02. Modern Times of (HashTag)method_every.js
@@ -0,0 +1,26 @@
+function modernTimesOfHashTag(str) {
+  str = str.split(" ");
+  for (const currentWord of str) {
+    if (currentWord[0] === "#" && currentWord.length > 1) {
+      let isValid = false;
+      let currentWordArray = currentWord.slice(1).split("");
+      currentWordArray.every((char) => {
+        if (
+          (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) ||
+          (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122)
+        ) {
+          return (isValid = true);
+        } else {
+          return (isValid = false);
+        }
+      });
+      if (isValid) {
+        console.log(currentWord.slice(1));
+      }
+    }
+  }
+}
+modernTimesOfHashTag("Nowadays everyone uses #* to tag a #special word in #socialMedia");
+modernTimesOfHashTag(
+  "The symbol # is known #variously in English-speaking #regions as the #number sign"
+);
