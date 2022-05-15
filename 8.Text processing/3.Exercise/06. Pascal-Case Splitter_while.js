e92c4af79a7caba730770ba0bad7bf2fd78843c7
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-05-15 10:55:53 +0300

    Add files via upload

---
+++ 8.Text processing/3.Exercise/06. Pascal-Case Splitter_while.js
@@ -0,0 +1,14 @@
+function solve(text) {
+  let result = text[0];
+
+  let lower = text.toLocaleLowerCase();
+
+  for (let i = 1; i < text.length; i++) {
+    if (text[i] != lower[i]) {
+      result += ", ";
+    }
+    result += text[i];
+  }
+  console.log(result);
+}
+solve("SplitMeIfYouCanHaHaYouCantOrYouCan");
