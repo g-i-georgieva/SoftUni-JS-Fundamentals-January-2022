dad17a3a69941a89943902a168bbb634b972578a
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-05-17 14:54:49 +0300

    Add files via upload

---
+++ 9.Regular Expressions/2.Lab/01. Match Full Name.js
@@ -0,0 +1,13 @@
+function solve(input) {
+    let pattern = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/g;
+    let result = [];
+  
+    let match = pattern.exec(input);
+  
+    while (match != null) {
+      result.push(match[0]);
+      match = pattern.exec(input);
+    }
+  
+    console.log(result.join(' '));
+  }
