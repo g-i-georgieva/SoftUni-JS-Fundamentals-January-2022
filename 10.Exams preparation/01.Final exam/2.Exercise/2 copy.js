174f2e1a7b1dc917f1d3b43dfab8099a261385b6
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-05-19 09:09:26 +0300

    Add files via upload

---
+++ 10.Exams preparation/01.Final exam/2.Exercise/2 copy.js
@@ -0,0 +1,26 @@
+function adAstra(input) {
+  let text = input[0];
+  let pattern =
+    /(#|\|)(?<name>[A-Za-z\s]+)\1(?<date>\d{2}\/\d{2}\/\d{2})\1(?<cal>\d{1,5})\1/g;
+  let match = pattern.exec(text);
+  let totalCal = 0;
+  let foodInfo = [];
+
+  while (match !== null) {
+    let name = match.groups["name"];
+    let date = match.groups["date"];
+    let cal = Number(match.groups["cal"]);
+    totalCal += cal;
+    foodInfo.push({ name, date, cal });
+    match = pattern.exec(text);
+  }
+
+  console.log(`You have food to last you for: ${Math.floor(totalCal/2000)} days!`);
+  
+  for (let el of foodInfo) {
+    console.log(`Item: ${el.name}, Best before: ${el.date}, Nutrition: ${el.cal}`);
+  }
+}
+adAstra([
+  "#Bread#19/03/21#4000#|Invalid|03/03.20||Apples|08/10/20|200||Carrots|06/08/20|500||Not right|6.8.20|5|",
+]);
