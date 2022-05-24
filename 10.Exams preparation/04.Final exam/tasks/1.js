a5081525c39b261a7bf8fb602fe43f993cf6146d
G. G <100354136+g-i-georgieva@users.noreply.github.com>
2022-05-24 21:07:42 +0300

    Add files via upload

---
+++ 10.Exams preparation/04.Final exam/tasks/1.js
@@ -0,0 +1,13 @@
+
+solve(["up8rgoyg3r1atmlmpiunagt!-irs7!1fgulnnnqy",
+    "TakeOdd",
+    "Cut 18 2",
+    "Substitute ! ***",
+    "Substitute ? .!.",
+    "Done"])
+solve(["Siiceercaroetavm!:?:ahsott.:i:nstupmomceqr",
+    "TakeOdd",
+    "Cut 15 3",
+    "Substitute :: -",
+    "Substitute | ^",
+    "Done"])
